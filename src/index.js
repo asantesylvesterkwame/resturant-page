1455595ec1c65df76606726bab1d72311df99f24
asantesylvesterkwame <intelligentson17@gmail.com>
2023-10-17 11:24:42 +0000

    created added and arranged elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-import  { Navbar }  from "../src/modules/Navbar.js";
+import bgImageFile from "../assets/bgImage.jpg";
+import ContactContent from "./components/ContactContent/ContactContent";
+import HomeContent from "./components/HomeContent/HomeContent";
+import MenuContent from "./components/MenuContent/MenuContent";
+import Navbar from "./components/Navbar/Navbar";
+import "./index.css";
 
-Navbar();
+const Home = () => {
+  let main = document.createElement("div");
+  main.id = "main";
+
+  const NavbarDiv = document.getElementById("NavbarDiv");
+
+  NavbarDiv.appendChild(Navbar());
+
+  const homeSection = document.getElementById("HomeSectionDiv");
+  homeSection.appendChild(HomeContent());
+
+  const menuSection = document.getElementById("menuSection");
+  menuSection.appendChild(MenuContent());
+  menuSection.style.display = "none";
+
+
+  const contactSection = document.getElementById("contactSection");
+  contactSection.appendChild(ContactContent());
+  contactSection.style.display = "none";
+
+  //   main.appendChild(MenuContent());
+
+  const footer = document.createElement("div");
+
+    // const image = new Image()
+    // image.src = bgImageFile
+    // image.className = "bgImage"
+    // main.appendChild(image)
+
+  return main;
+};
+
+const content = document.getElementById("content");
+content.appendChild(Home());
